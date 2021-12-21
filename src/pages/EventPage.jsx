76e0d3747025bc28001729ffb380dca35a3d2c13
Joel Wisdom <joelwisdom64@gmail.com>
2021-12-20 19:36:28 -0500

    Created EventPage

---
+++ src/pages/EventPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// functional component
+function EventPage() {
+
+    // renders the page
+    return (
+        <div className='EventPage'>
+            <h1>This is the EventPage</h1>
+        </div>
+    )
+}
+
+export default EventPage
