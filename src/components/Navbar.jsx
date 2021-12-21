726e808d09750dec6222d10724528e4018b0b526
Daryn Brown <darynnbrown@gmail.com>
2021-12-20 20:27:45 -0500

    Modified navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar() {
     return (
         <div className='Navbar'>
-            <h1>navbar here</h1>
+            <h1>navbar</h1>
         </div>
     )
 }
