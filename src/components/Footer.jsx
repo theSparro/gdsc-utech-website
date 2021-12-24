87ccdc21780a309f017410738cbfb114e718a01b
shawn-grant <shawngrant333@gmail.com>
2021-12-24 18:16:13 -0500

    fixed Footer position issue

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import './styles/footer.scss'
 
 function Footer() {
     return (
         <div className='Footer'>
-            
+            <h1>My </h1>
         </div>
     )
 }
