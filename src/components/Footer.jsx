ebe0a73441dc459f908e734c320f2d4f42df0117
Matthewsamuels20 <matthewsamuels26@gmail.com>
2021-12-24 18:24:56 -0800

    Changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import './styles/footer.scss'
+import logo from '../images/gdsc_logo.png'
 
 function Footer() {
     return (
         <div className='Footer'>
-            
+            <img src= {logo} alt="" width= '35px' height= '50px'/>
         </div>
     )
 }
