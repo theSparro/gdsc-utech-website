837718ddce640078a2cb5254a8fb5ffc7934a990
Garvain Campbell <GarvainACampbell@gmail.com>
2021-12-28 16:46:09 -0500

    Took out the divs and added the emoji

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import downArrow from '../images/down_arrow.png'
+import emoji from '../images/emoji.png'
 
 // import the CSS
 import './styles/registrationpage.scss'
@@ -14,45 +15,51 @@ function RegistrationPage() {
             <div className="sub">
                 To join the club, Please complete the following steps
             </div>
-
+ 
 
             <div className="subHead">
                 <h3>
-                     <span className='dot'><i className="bi bi-circle-fill"></i>
-                     </span>Join Community Platform
+                     <i id='dot' className="bi bi-circle-fill"></i>
+                     Join Community Platform
                 </h3>
-
                  <p id='text'>
                     Visit the link below and click the "Join" to become a member and be notified of our events.
                  </p>
 
-                 <div className='link'><a target='_blank' rel='noopener' href="https://gdsc.community.dev/university-of-technology/">Click Here</a> 
-                 </div>      
+                 <a target='_blank' rel='noopener' href="https://gdsc.community.dev/university-of-technology/">Click Here</a> 
+
+                <br />
+
+                    <img src={downArrow} id='down-arr' alt="" width={50}/>
+                
             </div>
 
-            <div className='arrow'><img src={downArrow} id='down-arr' alt="" width={50}/></div>
-
-            <div className="subHead2">
+            <div className="subHead">
                 <h3>
-                    <span className='dot'><i className="bi bi-circle-fill"></i>
-                    </span> Fill Out Our Survey
+                    <i id='dot' className="bi bi-circle-fill"></i>
+                     Fill Out Our Survey
                 </h3>
                 <p id='text'>
                      Complete the form below. You may join our Whatsapp group with the link provided on submission
                 </p>
-            <div className="link"><a target='_blank' rel='noopener' href="https://docs.google.com/forms/d/e/1FAIpQLSdukiMgRDsVNczSyKYMNEf78uc3821sDreEzkTDfoxirf5GcA/viewform">Fill Out Form</a></div>
+                 <a target='_blank' rel='noopener' href="https://docs.google.com/forms/d/e/1FAIpQLSdukiMgRDsVNczSyKYMNEf78uc3821sDreEzkTDfoxirf5GcA/viewform">Fill Out Form</a>
+
+                 <br />
+
+                    <img src={downArrow} id='down-arr' alt="" width={50}/>
+                
             </div>
             
-            
-            <div className='arrow'><img src={downArrow} id='down-arr' alt="" width={50}/></div>
             <br />
 
-            <div className="subHead3">
+            <div className="subHead">
                 <h3>
-                    <span className='dot'>
-                        <i className="bi bi-circle-fill"></i> 
-                    </span>Welcome To The Party</h3>
+                <i id='dot' className="bi bi-circle-fill"></i>
+                    Welcome To The Party <img id='emoji' src={emoji} alt="" /></h3> 
             </div>
+
+            <br />
+            <br />
        </div>
     )
 }
