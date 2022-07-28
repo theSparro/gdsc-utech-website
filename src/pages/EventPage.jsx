54c2a46a3d0466c6e45255fccaa2b1a4aeb13fed
Shawn Grant <shawngrant333@gmail.com>
2022-07-28 02:57:12 -0500

    ✅events page done

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -1,13 +1,69 @@
-import React from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react'
+import {
+	getFirestore,
+	collection,
+	getDocs,
+	query,
+	orderBy,
+} from 'firebase/firestore'
 // import { Link } from 'react-router-dom'
 import EventCard from '../components/EventCard.jsx'
 import PrimaryButton from '../components/PrimaryButton.jsx'
 
 // import css styles
 import './styles/eventpage.scss'
+// import { useNavigate } from 'react-router-dom'
 
 // functional component
 function EventPage() {
+	let eventType = {
+		dedicatedPageLink: '',
+		description: '',
+		startDate: Date,
+		endDate: Date,
+		eventLink: '',
+		shortTitle: '',
+		title: '',
+		videoLink: '',
+	}
+	const firestore = getFirestore()
+	const eventCollection = collection(firestore, 'events/')
+	let [allEvents, setAllEvents] = useState([eventType])
+	let [upcomingEvents, setUpcomingEvents] = useState([eventType])
+
+	useEffect(() => {
+		getEvents()
+	}, [])
+
+	const getEvents = async () => {
+		setAllEvents([])
+		setUpcomingEvents([])
+		try {
+			let result = await getDocs(
+				query(eventCollection, orderBy('startDate', 'desc'))
+			)
+			result.forEach((doc) => {
+				let eventObj = {
+					dedicatedPageLink: doc.data().dedicatedPageLink,
+					description: doc.data().description,
+					startDate: doc.data().startDate.toDate(),
+					endDate: doc.data().endDate.toDate(),
+					eventLink: doc.data().eventLink,
+					shortTitle: doc.data().shortTitle,
+					title: doc.data().title,
+					videoLink: doc.data().videoLink,
+				}
+
+				if (doc.data().startDate.toDate() > Date.now())
+					setUpcomingEvents((old) => [...old, eventObj])
+				else setAllEvents((old) => [...old, eventObj])
+			})
+		} catch (e) {
+			console.log(e)
+		}
+	}
+
 	// renders the page
 	return (
 		<div className="EventPage">
@@ -15,61 +71,48 @@ function EventPage() {
 			<h1>Our Events</h1>
 			<br />
 			<section className="upcoming">
-				<h3 className="section-head">Upcoming</h3>
-				<div className="event-box">
-					<div className="left">
-						<EventCard title="Intro to Machine Learning" />
-					</div>
+				<h4 className="section-head">Upcoming</h4>
 
-					<div className="right">
-						<h2>Introduction to Machine Learning</h2>
-						<h4>20/01/2022 - 3PM</h4>
-						<br />
-						<p>
-							This event will give a clear explanation of Machine
-							Learning, and its importance in predictions.
-							<br />
-							The speaker will demonstrate, and students will use
-							Python to build a model from scratch.
-						</p>
-						<PrimaryButton
-							onClick={() => console.log('test')}
-							text="RSVP"
-							color="green"
-						/>
-					</div>
+				<div className="list">
+					{upcomingEvents.length === 0 && (
+						<div className="event-box">No upcoming events</div>
+					)}
+					{upcomingEvents.map((event, i) => (
+						<div className="event-box" key={i}>
+							<div className="left">
+								<EventCard title={event.shortTitle} />
+							</div>
+
+							<div className="right">
+								<h2>{event.title}</h2>
+								<h4>{event.startDate.toLocaleString()}</h4>
+								<br />
+								<p>{event.description}</p>
+								<PrimaryButton
+									onClick={() =>
+										window.open(event.eventLink, '_blank')
+									}
+									text="RSVP"
+									color="green"
+								/>
+							</div>
+						</div>
+					))}
 				</div>
 				<br />
 				<br />
 			</section>
 
 			<section className="previous">
-				<h3 className="section-head">Previous Events</h3>
+				<h4 className="section-head">Past Events</h4>
 
-				<EventCard
-					title="Intro To Machine Learning"
-					link="https://youtu.be/OYhFIx8EYpM"
-				/>
-				<EventCard
-					title="Intro To GitHub"
-					link="https://youtu.be/TuszwdwJwBY"
-				/>
-				<EventCard
-					title="Intro To Firebase"
-					link="https://youtu.be/RBuNQAmDDU0"
-				/>
-				<EventCard
-					title="Intro To Flutter"
-					link="https://youtu.be/JgsZDxuQLzY"
-				/>
-				<EventCard
-					title="Design Thinking"
-					link="https://youtu.be/OuXAgXLe8Hs"
-				/>
-				<EventCard
-					title="Career Talk"
-					link="https://youtu.be/aXnY375U6BI"
-				/>
+				{allEvents.map((event, i) => (
+					<EventCard
+						key={i}
+						title={event.shortTitle}
+						link={event.eventLink}
+					/>
+				))}
 			</section>
 
 			<div className="community">
